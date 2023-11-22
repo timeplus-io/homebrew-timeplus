b830534c7410195031c1af29fb4e81c381b6dd77
yokofly <haohang.shi@timeplus.io>
2023-11-22 11:18:48 +0800

    update proton@1.3.24 ver

---
+++ Formula/proton@1.3.24.rb
@@ -0,0 +1,45 @@
+class ProtonAT1324 < Formula
+    desc "A unified streaming and historical data processing engine in one single binary, powered by ClickHouse"
+    homepage "https://timeplus.com"
+    license "Apache-2.0"
+
+    version "1.3.24"
+
+    if Hardware::CPU.arm?
+      url "https://github.com/timeplus-io/proton/releases/download/v#{version}/proton-v#{version}-Darwin-arm64"
+      sha256 "5990b70cc34eb2715b8fdd7cd802a181241df5dfc9d8be70dad7793f4326767b"
+    else
+      url "https://github.com/timeplus-io/proton/releases/download/v#{version}/proton-v#{version}-Darwin-x86_64"
+      sha256 "5842e1cff93503fe420f5414633fc9d7f5906439749c962273a24b82dc20ef9b"
+    end
+
+    def install
+      if Hardware::CPU.arm?
+        bin.install "proton-v#{version}-Darwin-arm64" => "proton"
+      elsif Hardware::CPU.intel?
+        bin.install "proton-v#{version}-Darwin-x86_64" => "proton"
+      end
+    end
+
+    def caveats
+        <<~EOS
+          To interact with Proton:
+
+          1. Start the Proton server:
+             $ proton server start
+
+          2. In a separate terminal, connect to the server:
+             $ proton client
+             (Note: If you encounter a 'connection refused' error, use: proton client --host 127.0.0.1)
+
+          3. To terminate the server, press ctrl+c in the server terminal.
+
+          For detailed usage and more information, check out the Timeplus documentation:
+          https://docs.timeplus.com/
+        EOS
+    end
+
+    test do
+      assert_match "proton-robot", shell_output("#{bin}/proton local --query 'SELECT * FROM system.contributors'")
+    end
+  end
