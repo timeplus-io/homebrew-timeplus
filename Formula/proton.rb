9e26dfaf7198b7a02f1e29da56d29bfd8f19d960
Jove Zhong <jove@timeplus.io>
2024-01-21 14:31:59 -0800

    Update proton.rb for 1.4.1

--- Formula/proton.rb
+++ Formula/proton.rb
@@ -3,14 +3,14 @@ class Proton < Formula
     homepage "https://timeplus.com"
     license "Apache-2.0"
 
-    version "1.3.31"
+    version "1.4.1"
 
     if Hardware::CPU.arm?
       url "https://github.com/timeplus-io/proton/releases/download/v#{version}/proton-v#{version}-Darwin-arm64"
-      sha256 "a7902ed4316ceee3edda422cfdd8ffd48a20a2ad5e6cf9584404c39e27bb32a4"
+      sha256 "df4f5901b6277903b04f11e2bb4ac96de0607ee0737b71b9af9984428b7d2efa"
     else
       url "https://github.com/timeplus-io/proton/releases/download/v#{version}/proton-v#{version}-Darwin-x86_64"
-      sha256 "201c973f409497fdd8df7e8920c19d6f325e4a4352c5d9aa8810eef72b684e3e"
+      sha256 "df4f5901b6277903b04f11e2bb4ac96de0607ee0737b71b9af9984428b7d2efa"
     end
 
     def install
